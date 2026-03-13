834f1dffb15afed7b7bd8772e2afdadc67330b9f
Ssssserge <yanchevskiy07@gmail.com>
2026-03-13 22:19:56 +0300

    Все задания к 4 занятию

--- solutions/sem02/lesson04/task1.py
+++ solutions/sem02/lesson04/task1.py
@@ -2,16 +2,49 @@ import numpy as np
 
 
 def pad_image(image: np.ndarray, pad_size: int) -> np.ndarray:
-    # ваш код
-    return image
+    if pad_size < 1:
+        raise ValueError
+    if image.ndim == 2:
+        h, w = image.shape
+        shape_with_frame = (h + 2 * pad_size, w + 2 * pad_size)
+    if image.ndim == 3:
+        h, w, s = image.shape
+        shape_with_frame = (h + 2 * pad_size, w + 2 * pad_size, s)
+
+    result = np.zeros(shape_with_frame, dtype=image.dtype)
+
+    result[pad_size : pad_size + h, pad_size : pad_size + w] = image
+
+    return result
 
 
 def blur_image(
     image: np.ndarray,
     kernel_size: int,
 ) -> np.ndarray:
-    # ваш код
-    return image
+    if kernel_size < 1 or kernel_size % 2 == 0:
+        raise ValueError
+
+    if kernel_size == 1:
+        return image.copy()
+
+    pad_size = kernel_size // 2
+    padded = pad_image(image, pad_size)
+    if image.ndim == 2:
+        h, w = image.shape
+    if image.ndim == 3:
+        h, w, _ = image.shape
+
+    result = np.zeros(image.shape, dtype=float)
+
+    for i in range(h):
+        for j in range(w):
+            window = padded[i : i + kernel_size, j : j + kernel_size]
+            if image.ndim == 2:
+                result[i, j] = np.mean(window)
+            else:
+                result[i, j] = np.mean(window, axis=(0, 1))
+    return result.round().astype(image.dtype)
 
 
 if __name__ == "__main__":
--- solutions/sem02/lesson04/task2.py
+++ solutions/sem02/lesson04/task2.py
@@ -5,6 +5,28 @@ def get_dominant_color_info(
     image: np.ndarray[np.uint8],
     threshold: int = 5,
 ) -> tuple[np.uint8, float]:
-    # ваш код
+    if threshold < 1:
+        raise ValueError("threshold must be positive")
 
-    return 0, 0
+    flat = image.ravel()
+
+    if len(flat) == 0:
+        return 0.0
+
+    unique_colors = np.unique(flat)
+
+    best_color = 0
+    max_count = 0
+
+    for color in unique_colors:
+        lower = max(0, int(color) - threshold + 1)
+        upper = min(255, int(color) + threshold - 1)
+
+        count = np.sum((flat >= lower) & (flat <= upper))
+
+        if count > max_count:
+            max_count = count
+            best_color = color
+
+    percent = (max_count / len(flat)) * 100.0
+    return best_color, percent
